ef45d915b01e264cfc5c9336df1f5c7f4df057b3
subrinalisa14 <subrina@aci-bd.com>
2026-06-18 12:43:19 +0600

    Remove node_modules from tracking

--- node_modules/react-zoom-pan-pinch/src/hooks/use-controls.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactZoomPanPinchContentRef } from "models";
-import { getControls } from "utils";
-import { useTransformContext } from "./use-transform-context";
-
-export const useControls = (): ReactZoomPanPinchContentRef => {
-  const libraryContext = useTransformContext();
-
-  return getControls(libraryContext);
-};
--- node_modules/react-zoom-pan-pinch/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./hooks";
-export * from "./components";
-
-export * from "./utils/styles.utils";
-export * from "./models/context.model";
-export * from "./models/calculations.model";
